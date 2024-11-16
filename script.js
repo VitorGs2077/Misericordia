225f270095cd8d0b9cdd78f1fa812492fff41f13
VitorGs2077 <vitor.diniz20008@gmail.com>
2024-11-16 10:24:44 -0300

    Mudanças nas margens

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+document.getElementById('bt2').style.visibility = 'hidden'
+document.getElementById('bt3').style.visibility = 'hidden'
 function misericordia() {
     const audio = new Audio("porminhavida.mp3")
     audio.play()
     document.getElementById("titulo").innerHTML = ""
-    document.getElementById('bt').style.visibility = 'hidden';
+    document.getElementById('bt').style.visibility = 'hidden'
     document.getElementById("img1").src = "img/🐘🍓.jpg"
+    document.getElementById('bt2').style.visibility = 'visible'
+    document.getElementById('bt3').style.visibility = 'visible'
 }
